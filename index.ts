5b12c31e2c86a25df65788f5c41a5e1c2f21327e
kks91 <kokyusik91@naver.com>
2022-08-01 21:45:27 +0900

    set lastest files

--- index.ts
+++ index.ts
@@ -8,9 +8,14 @@ import {domInit} from './src/loaded'
 import {clicker} from './src/paragraph';
 import {titleChange} from './src/title';
 
+declare var API_DOMAIN : string | undefined;
+
 window.addEventListener('DOMContentLoaded', function(){
   domInit();
   clicker();
   titleChange();
   console.log(sum(9999,1));
 })
+
+console.log(process.env.NODE_ENV);
+console.log(API_DOMAIN);
